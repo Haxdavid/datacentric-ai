a41d7468d23f089e977432312c59e8dec3d16501
David Hax <hax.david@web.de>
2025-08-20 15:51:14 +0200

    Parallelisation of experiments

---
+++ scripts/exp_2.py
@@ -0,0 +1,18 @@
+import os 
+from src.utils import load_and_expand_yaml, logger, RESULTS_DIR
+from src.current_experiment import Experiment
+
+print(__name__)
+
+if __name__ == "__main__":
+    config_path = "experiments/experiment_FreshPRINCE_32.yaml"
+    base_path = "112UCRFolds"
+
+    os.makedirs(RESULTS_DIR, exist_ok=True)
+    configs = load_and_expand_yaml(config_path)
+
+    for config in configs:
+        experiment = Experiment(config, base_path=base_path, results_root=RESULTS_DIR)
+        experiment.run_experiment()
+
+    logger.info("All experiments completed")
